43cc488f2fa22e4892a4db118ec86d047c9248ae
Umut Karaytu <128383118+Karaytumut20@users.noreply.github.com>
2024-08-07 20:06:28 +0300

    sdaaaaaaaaaaaaaa

--- app/components/cubeCard/cubeCard.js
+++ app/components/cubeCard/cubeCard.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
