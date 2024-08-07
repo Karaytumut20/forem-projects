c04e7d132a58f13018b4a1ec2601cb9c33c3f3e0
Umut Karaytu <128383118+Karaytumut20@users.noreply.github.com>
2024-08-07 19:59:31 +0300

    sdadadadada

--- app/components/cube/cube.js
+++ app/components/cube/cube.js
@@ -1,40 +1,39 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
-import { makeStyles } from "@mui/styles";
+import styled from "@emotion/styled";
 
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "flex-start",
-    minHeight: "25vh",
-  },
-  cube: {
-    width: "90px",
-    height: "90px",
-    position: "relative",
-    transformStyle: "preserve-3d",
-    transform: "rotateY(-47deg) rotateX(-15deg) rotateZ(15deg) translateX(50%)",
-  },
-  face: {
-    width: "90px",
-    height: "90px",
-    position: "absolute",
-    boxShadow: "inset 0px 0 1px 2px black",
-    border: "0.0001px solid black",
-  },
-});
-//sdadasdadas
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  min-height: 25vh;
+`;
+
+const CubeWrapper = styled.div`
+  width: 90px;
+  height: 90px;
+  position: relative;
+  transform-style: preserve-3d;
+  transform: rotateY(-47deg) rotateX(-15deg) rotateZ(15deg) translateX(50%);
+`;
+
+const Face = styled.div`
+  width: 90px;
+  height: 90px;
+  position: absolute;
+  box-shadow: inset 0px 0 1px 2px black;
+  border: 0.0001px solid black;
+`;
+
 const Cube = ({ frontColor, topColor, rightColor }) => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.container}>
-      <div className={classes.cube}>
-        <div className={`${classes.face}`} style={{ background: frontColor, transform: 'translateZ(45px)', filter: 'brightness(0.9)' }}></div>
-        <div className={`${classes.face}`} style={{ background: topColor, transform: 'rotateX(90deg) translateZ(45px)', filter: 'brightness(1.36)' }}></div>
-        <div className={`${classes.face}`} style={{ background: rightColor, transform: 'rotateY(90deg) translateZ(45px)', filter: 'brightness(0.9)' }}></div>
-      </div>
-    </div>
+    <Container>
+      <CubeWrapper>
+        <Face style={{ background: frontColor, transform: 'translateZ(45px)', filter: 'brightness(0.9)' }} />
+        <Face style={{ background: topColor, transform: 'rotateX(90deg) translateZ(45px)', filter: 'brightness(1.36)' }} />
+        <Face style={{ background: rightColor, transform: 'rotateY(90deg) translateZ(45px)', filter: 'brightness(0.9)' }} />
+      </CubeWrapper>
+    </Container>
   );
 };
 
